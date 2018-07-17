f8ce8d50dee6da3ce3054e6d632842a180b3f103
chendong <chendong@plusev.cn>
2018-07-17 16:04:19 +0800

    no message

--- CustomTabbar/CustomTabbar/CDTabbar/CDTabBar.m
+++ CustomTabbar/CustomTabbar/CDTabbar/CDTabBar.m
@@ -10,6 +10,8 @@
 
 @interface CDTabBar()
 @property (nonatomic, strong) UIButton *middleBtn;
+@property (nonatomic, strong) UIButton *sendBtn;
+
 @end
 
 @implementation CDTabBar
@@ -17,22 +19,10 @@
 - (void)layoutSubviews
 {
     [super layoutSubviews];
-    CGFloat w = self.bounds.size.width/5.0;
+    [self addSubview:self.sendBtn];
     
-    UIButton *sendBtn = [[UIButton alloc] init];
-    [sendBtn setImage:[UIImage imageNamed:@"tabar_plus_normal"] forState:UIControlStateNormal];
-    [sendBtn setTitle:@"发布" forState:UIControlStateNormal];
-    sendBtn.titleLabel.font = [UIFont systemFontOfSize:10];
-    [sendBtn addTarget:self action:@selector(didClickPublishBtn:) forControlEvents:UIControlEventTouchUpInside];
-    sendBtn.adjustsImageWhenHighlighted = NO;
-    sendBtn.size = CGSizeMake(w, 70);
-    sendBtn.centerX = self.width / 2;
-    sendBtn.centerY = 12;
-    [sendBtn setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
-    [self addSubview:sendBtn];
-    self.middleBtn = sendBtn;
     
-    [sendBtn setImagePositionWithType:SSImagePositionTypeTop spacing:4];
+    [self.sendBtn setImagePositionWithType:SSImagePositionTypeTop spacing:4];
     // 其他位置按钮
     NSUInteger count = self.subviews.count;
     for (NSUInteger i = 0 , j = 0; i < count; i++)
@@ -76,6 +66,23 @@
     {
         return [super hitTest:point withEvent:event];
     }
+}
+
+- (UIButton *)sendBtn{
+    if (!_sendBtn) {
+        _sendBtn = [[UIButton alloc] init];
+        [_sendBtn setImage:[UIImage imageNamed:@"tabar_plus_normal"] forState:UIControlStateNormal];
+        [_sendBtn setTitle:@"发布" forState:UIControlStateNormal];
+        _sendBtn.titleLabel.font = [UIFont systemFontOfSize:10];
+        [_sendBtn addTarget:self action:@selector(didClickPublishBtn:) forControlEvents:UIControlEventTouchUpInside];
+        _sendBtn.adjustsImageWhenHighlighted = NO;
+        _sendBtn.size = CGSizeMake(self.bounds.size.width/5.0, 70);
+        _sendBtn.centerX = self.width / 2;
+        _sendBtn.centerY = 12;
+        [_sendBtn setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
+        self.middleBtn = _sendBtn;
+    }
+    return _sendBtn;
 }
 
 @end
